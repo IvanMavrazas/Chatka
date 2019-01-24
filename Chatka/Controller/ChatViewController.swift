4c9bac2626643aeb8d3e73ea29e862a9f6882380
Ivan Mavrazas <mavrazas@gmail.com>
2019-01-24 19:04:07 +0100

    Set textFieldBeginEditing and textFieldEndEditing methods

--- Chatka/Controller/ChatViewController.swift
+++ Chatka/Controller/ChatViewController.swift
@@ -11,26 +11,36 @@ import Firebase
 import SVProgressHUD
 import ChameleonFramework
 
-class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
     
     let messages: [String] = ["firstmessage", "Secondmessage"]
-
+    
+    
     @IBOutlet weak var messageTextView: UITextField!
     @IBOutlet weak var sendMessageButton: UIButton!
     
     @IBOutlet weak var messageTableView: UITableView!
     
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         messageTableView.delegate = self
         messageTableView.dataSource = self
-        
+        messageTextView.delegate = self
         
         messageTableView.register(UINib(nibName: "CustomMessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
     }
     
 
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        heightConstraint.constant = 308
+        view.layoutIfNeeded()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
---
+++ Chatka/Model/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Chatka
+//
+//  Created by Ivan Mavrazas on 1/23/19.
+//  Copyright © 2019 Ivan Mavrazas. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    var sender: String = ""
+    var messageText: String = ""
+    
+}

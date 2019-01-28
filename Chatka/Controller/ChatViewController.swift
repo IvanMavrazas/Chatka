da0792380f20d70ff70a57e2b805f9fa1039c14e
Ivan Mavrazas <mavrazas@gmail.com>
2019-01-28 21:44:33 +0100

    sendButton configuration complete.

--- Chatka/Controller/ChatViewController.swift
+++ Chatka/Controller/ChatViewController.swift
@@ -13,7 +13,7 @@ import ChameleonFramework
 
 class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
     
-    let messages: [String] = ["firstmessage", "Secondmessage"]
+    var messages: [Message] = [Message]()
     
     
     @IBOutlet weak var messageTextView: UITextField!
@@ -33,12 +33,14 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         messageTableView.addGestureRecognizer(tapGesture)
         
         messageTableView.register(UINib(nibName: "CustomMessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        retrieveMessages()
     }
     
 
     func textFieldDidBeginEditing(_ textField: UITextField) {
 
-        UIView.animate(withDuration: 1) {
+        UIView.animate(withDuration: 0.5) {
             
             self.heightConstraint.constant = 308
             self.view.layoutIfNeeded()
@@ -60,11 +62,54 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
-        cell.messageBody.text = messages[indexPath.row]
+        cell.messageBody.text = messages[indexPath.row].messageText
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return messages.count
+    }
+    
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        messageTextView.endEditing(true)
+        messageTextView.isEnabled = false
+        sendMessageButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextView.text!]
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error,reference) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message saved successfully!")
+            }
+            
+            self.messageTextView.isEnabled = true
+            self.sendMessageButton.isEnabled = true
+            self.messageTextView.text = nil
+        }
+    }
+    
+    func retrieveMessages() {
+        let messageDB = Database.database().reference().child("Messages")
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            
+            let snapshotValue = snapshot.value as! Dictionary<String,String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageText = text
+            message.sender = sender
+            
+            self.messages.append(message)
+            
+            self.messageTableView.reloadData()
+        }
     }
 }
